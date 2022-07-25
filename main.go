ffd11e02b2818826eccf091047b6bc8eec74d0ce
fhrw <felix.dork@gmail.com>
2022-07-25 13:30:00 +1000

    basically working, no output yet

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"text/tabwriter"
 )
 
 type job struct {
@@ -29,26 +30,57 @@ func main() {
 	employer, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	} else {
+		fmt.Println("Okay great! What should I invoice ", strings.TrimSpace(employer), " for?")
+		jobName, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Okay great! How much should I invoice for ", strings.TrimSpace(jobName))
+		cost, err := reader.ReadString('\n')
+		convertCost, err := strconv.ParseFloat(strings.TrimSpace(cost), 64)
+		job := job{jobName: strings.TrimSpace(jobName), cost: convertCost}
+		invoice := invoice{name: employer, jobs: []string{job.jobName}, costs: []float64{job.cost}}
+
+		done := checkDone()
+		for !done {
+			jobName, cost := otherItems(invoice)
+			invoice.jobs = append(invoice.jobs, jobName)
+			invoice.costs = append(invoice.costs, cost)
+			done = checkDone()
+		}
+		jobs := invoice.jobs
+		costs := invoice.costs
+		createTable(jobs, costs, employer)
 	}
+}
 
-	fmt.Println("Okay great! What should I invoice ", strings.TrimSpace(employer), " for?")
-	jobName, err := reader.ReadString('\n')
-
-	fmt.Println("Okay great! How much should I invoice for ", strings.TrimSpace(jobName))
-	cost, err := reader.ReadString('\n')
-	convertCost, err := strconv.ParseFloat(strings.TrimSpace(cost), 64)
-	job := job{jobName: strings.TrimSpace(jobName), cost: convertCost}
-	invoice := invoice{name: employer, jobs: []string{job.jobName}, costs: []float64{job.cost}}
-
-	done := checkDone()
-	for !done {
-		jobName, cost := otherItems(invoice)
-		invoice.jobs = append(invoice.jobs, jobName)
-		invoice.costs = append(invoice.costs, cost)
-		done = checkDone()
+func createTable(jobs []string, costs []float64, employer string) {
+	// nf, err := os.Create("newfile.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer nf.Close()
+	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 1, '\t', tabwriter.AlignRight)
+	length := len(jobs)
+	fmt.Fprintln(writer, "Invoice to\t"+employer)
+	fmt.Println("----------")
+	fmt.Fprintln(writer, "Job\tUnit Cost")
+	for i := 0; i < length; i++ {
+		output := jobs[i] + "\t" + fmt.Sprintf("%.2f", costs[i]) + "\n"
+		fmt.Fprintln(writer, output)
 	}
-
-	fmt.Println(invoice)
+	total := 0.00
+	for i := 0; i < length; i++ {
+		total += costs[i]
+	}
+	fmt.Fprintln(writer, "total\t"+fmt.Sprintf("%g", total))
+	fmt.Println("----------")
+	fmt.Fprintln(writer, "Name: \tFelix Watson")
+	fmt.Fprintln(writer, "BSB: \t033091")
+	fmt.Fprintln(writer, "ACC: \t349128")
+	writer.Flush()
 }
 
 func otherItems(invoice) (string, float64) {
