9a3078141dbedbf22cee3f8d32bc697f4275354a
fhrw <felix.dork@gmail.com>
2022-09-09 14:11:47 +1000

    mostly finished - outputs a file now

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"strings"
 	"text/tabwriter"
+	"time"
 )
 
 type job struct {
@@ -25,7 +26,7 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("Hey there!")
-	fmt.Println("So you'd like to make an invoice?")
+	fmt.Println("So you'd like to make an invoice...")
 	fmt.Println("Who should I make it out to?")
 	employer, err := reader.ReadString('\n')
 	if err != nil {
@@ -57,29 +58,38 @@ func main() {
 }
 
 func createTable(jobs []string, costs []float64, employer string) {
-	// nf, err := os.Create("newfile.txt")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer nf.Close()
-	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 1, '\t', tabwriter.AlignRight)
+	currDate := time.Now().Format("20060102150405")
+	printDate := currDate[0:10]
+	fN := strings.TrimSpace(employer) + "-" + printDate
+	_, err := os.Create(fN + ".txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output, err := os.OpenFile(fN+".txt", os.O_APPEND|os.O_WRONLY, 0644)
+	defer output.Close()
+
+	writer := tabwriter.NewWriter(output, 0, 8, 1, '\t', tabwriter.AlignRight)
 	length := len(jobs)
-	fmt.Fprintln(writer, "Invoice to\t"+employer)
-	fmt.Println("----------")
+
+	fmt.Fprintln(writer, "Invoice to\t"+strings.TrimSpace(employer))
+	fmt.Fprintln(writer, "\t"+"\t")
 	fmt.Fprintln(writer, "Job\tUnit Cost")
 	for i := 0; i < length; i++ {
-		output := jobs[i] + "\t" + fmt.Sprintf("%.2f", costs[i]) + "\n"
+		output := jobs[i] + "\t" + fmt.Sprintf("%.2f", costs[i])
 		fmt.Fprintln(writer, output)
 	}
 	total := 0.00
 	for i := 0; i < length; i++ {
 		total += costs[i]
 	}
-	fmt.Fprintln(writer, "total\t"+fmt.Sprintf("%g", total))
-	fmt.Println("----------")
+	fmt.Fprintln(writer, "\t"+"\t")
+	fmt.Fprintln(writer, "total:\t"+fmt.Sprintf("%g", total))
+	fmt.Fprintln(writer, "\t"+"\t")
 	fmt.Fprintln(writer, "Name: \tFelix Watson")
 	fmt.Fprintln(writer, "BSB: \t033091")
 	fmt.Fprintln(writer, "ACC: \t349128")
+	fmt.Fprintln(writer, "ABN: \t52048795150")
 	writer.Flush()
 }
 
@@ -94,7 +104,7 @@ func otherItems(invoice) (string, float64) {
 	cost, err := reader2.ReadString('\n')
 	convertCost, err := strconv.ParseFloat(strings.TrimSpace(cost), 64)
 
-	return jobName, convertCost
+	return strings.TrimSpace(jobName), convertCost
 }
 
 func checkDone() bool {
